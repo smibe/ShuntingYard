9113fec4fd1bb040fb79731734983c3bd0138948
Gerhard <geh@geherbert.de>
2013-01-31 10:22:21 +0100

    Initial commit
    TDD exerciser for doing the shunting yard algorithm

---
+++ InfixToPostfix/ShuntingYardAlgorithmTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InfixToPostfix
+{
+    [TestClass]
+    public class ShuntingYardAlgorithmTest
+    {
+        [TestMethod]
+        public void nothing()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
